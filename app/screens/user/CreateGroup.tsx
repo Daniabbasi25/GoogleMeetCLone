58c0a8025ef74f182f877fe02f9457672a3dbac7
Daniabbasi25 <danyal.munir@hysetech.net>
2024-08-05 17:24:49 +0500

    group creating and showing is done

--- app/screens/user/CreateGroup.tsx
+++ app/screens/user/CreateGroup.tsx
@@ -3,33 +3,39 @@ import React, {useState} from 'react';
 import {TextInput, Button, Snackbar} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 const CreateGroupScreen = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [name, setname] = useState<string>('');
 
   const [snackMessage, setSnackMessage] = useState<string | undefined>(
     undefined,
   );
 
-  const handleLogin = async (): Promise<void> => {
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        setSnackMessage('User signed in!');
-      })
-      .catch(error => {
-        setSnackMessage(String(error));
-      });
+  const handleSave = async (): Promise<void> => {
+    try {
+      const userDoc = {
+        name: name,
+        image: `https://avatar.iran.liara.run/username?username=${name}`,
+      };
+
+      await firestore().collection('Groups').add(userDoc);
+
+      setSnackMessage('Group Created Successful');
+    } catch (error) {
+      console.error('Error during Creating Group:', error);
+      setSnackMessage(String(error));
+      navigation.goBack();
+    }
   };
 
   return (
     <View style={styles.screenContainer}>
       <Text style={styles.heading}>Create Group</Text>
       <TextInput
-        value={email}
-        onChangeText={e => setEmail(e)}
+        value={name}
+        onChangeText={e => setname(e)}
         mode="outlined"
         label="Name"
         placeholder="Type Name here"
@@ -41,7 +47,7 @@ const CreateGroupScreen = () => {
 
       <Button
         mode="contained"
-        onPress={() => handleLogin()}
+        onPress={() => handleSave()}
         style={{borderRadius: 5}}>
         Save
       </Button>
--- app/screens/user/GroupListScreen.tsx
+++ app/screens/user/GroupListScreen.tsx
@@ -1,44 +1,61 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import Icon from 'react-native-vector-icons/Entypo';
 import firestore from '@react-native-firebase/firestore';
 import {useNavigation, useTheme} from '@react-navigation/native';
+import {Avatar, Card, IconButton} from 'react-native-paper';
 
 const GroupListScreen = () => {
   const [data, setData] = useState<any[]>([]);
   const navigation = useNavigation();
   const {colors} = useTheme();
   useEffect(() => {
-    const ALLDATA: any[] = [];
-    firestore()
+    const unsubscribe = firestore()
       .collection('Groups')
-      .get()
-      .then(querySnapshot => {
-        console.log('Total Groups: ', querySnapshot.size);
+      .onSnapshot(
+        querySnapshot => {
+          const ALLDATA = [];
+          querySnapshot.forEach(documentSnapshot => {
+            ALLDATA.push({id: documentSnapshot.id, ...documentSnapshot.data()});
+          });
+          setData(ALLDATA);
+          console.log('Updated Groups: ', ALLDATA);
+        },
+        error => {
+          console.error('Error fetching real-time updates: ', error);
+        },
+      );
 
-        querySnapshot.forEach(documentSnapshot => {
-          console.log(
-            'User ID: ',
-            documentSnapshot.id,
-            documentSnapshot.data(),
-          );
-          ALLDATA.push(documentSnapshot.id, documentSnapshot.data());
-        });
-      });
-    setData(ALLDATA);
+    // Cleanup the listener on unmount
+    return () => unsubscribe();
   }, []);
+
   return (
     <View style={{flex: 1}}>
-      <TouchableOpacity
+      {/* <TouchableOpacity
         onPress={() => {
           auth()
             .signOut()
             .then(() => console.log('User signed out!'));
         }}>
         <Text>UserStack = {auth().currentUser?.email}</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({item, index}) => (
+          <View style={styles.card}>
+            <Card.Title
+              title={item?.name}
+              left={props => (
+                <Avatar.Image {...props} source={{uri: item.image}} />
+              )}
+            />
+          </View>
+        )}
+      />
       <TouchableOpacity
         onPress={() => navigation.navigate('CreateGroupScreen' as never)}
         style={[styles.floatingButton, {backgroundColor: colors.primary}]}>
@@ -65,5 +82,12 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card: {
+    borderWidth: 1,
+    width: '95%',
+    alignSelf: 'center',
+    marginTop: 5,
+    borderBlockColor: 'gray',
   },
 });
